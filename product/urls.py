e7f00e7d76572b483ee1ac5fcfd1167863c13fdc
cosmus-bags-pvt-ltd <cosmusbags547@gmail.com>
2025-02-28 17:51:53 +0530

    working on delivery challlan

--- product/urls.py
+++ product/urls.py
@@ -416,4 +416,5 @@ urlpatterns = [
     path('deliverychallanupdate/<int:d_id>/', views.delivery_challan_create_update, name='delivery-challan-update'),
     path('deliverychallanlist/', views.delivery_challan_list, name='delivery-challan-list'),
     path('deliverychallanproductajax/', views.delivery_challan_product_ajax, name='delivery-challan-product-ajax'),
+    path('deletedeliverychallan/<int:pk>/', views.delete_delivery_challan, name='delete-delivery-challan'),
 ]
